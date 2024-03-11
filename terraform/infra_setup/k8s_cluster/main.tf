f7846bc141c718a1064e222a4ce020a06f1fb60a
cocoa-maemae <breezin_ahimusa@aol.com>
2024-03-11 16:34:03 +0900

    fix a k8s container name

--- terraform/infra_setup/k8s_cluster/main.tf
+++ terraform/infra_setup/k8s_cluster/main.tf
@@ -199,7 +199,9 @@ locals {
           containers = [
             {
               name = "boundary-worker"
-              image = "hashicorp/boundary-worker-hcp"
+              # not suppported image
+              # image = "hashicorp/boundary-worker-hcp"
+              image = "hashicorp/boundary-enterprise"
               command = [ "boundary-worker" ]
               args = [ "server", "-config", "/etc/boundary/boundary-worker-config" ]
               securityContext = {
