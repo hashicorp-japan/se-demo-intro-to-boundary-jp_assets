ea13d57649607f5dc19e70cb8f0150ee1cb0d4b8
cocoa-maemae <breezin_ahimusa@aol.com>
2024-03-11 16:08:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# se-demo-intro-to-boundary_assets
+Initial development of Terraform code for an Intro to Boundary Instruqt track.
---
+++ terraform/hcp/variables.tf
@@ -0,0 +1,9 @@
+variable "unique_name" {
+  type = string
+  default = ""
+}
+
+variable "boundary_admin_login" {
+  type = string
+  default = "admin"
+}
